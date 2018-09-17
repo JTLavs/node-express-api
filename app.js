cf329f26bd45f3f11c91511bc1e070d0a2b4b5f2
James Lavery <jamestlavery@gmail.com>
2018-09-17 19:01:36 +0100

    Testing Db connection

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const express = require('express')
+const express = require('express');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'myawsdb.cpemae4exgq9.eu-west-1.rds.amazonaws.com',
+  user : 'jameslavery',
+  password:'awsdatabase123',
+  database:'cities'
+});
 
 var app = express();
 
 app.get('/', function(req,res){
-  res.send('Hello AWS EB!. Pipeline is working!');
+  connection.connect(function(err){
+    if(err)
+      throw err;
+    else
+      res.send('Connection successful!');
+  });
 })
+
+
 
 app.listen(8081);
