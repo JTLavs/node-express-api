caf68e438b38069bb52ab32d41540c2da2dd6be1
James Lavery <jamestlavery@gmail.com>
2018-09-17 20:15:17 +0100

    Fixed RDS

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const express = require('express');
 var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: process.env.RDS_DB_HOST,
-  user : process.env.RDS_USER,
+  user: process.env.RDS_USER,
   password: process.env.RDS_PASSWORD,
-  database:process.env.RDS_DB_NAME,
-  port:process.env.RDS_PORT
+  database:process.env.RDS_DB_NAME
 });
 
 var app = express();
