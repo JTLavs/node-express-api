ae1655d008e26d79a464a39bf8122488e3f13bc7
James Lavery <jamestlavery@gmail.com>
2018-09-17 18:23:29 +0100

    Pipeline test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 var app = express();
 
 app.get('/', function(req,res){
-  res.send('Hello AWS EB!');
+  res.send('Hello AWS EB!. Pipeline is working!');
 })
 
 app.listen(8081);
