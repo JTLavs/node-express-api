d69b0ebe38ed7bb7d2169650615a0e77f741b009
James Lavery <jamestlavery@gmail.com>
2018-09-17 19:46:12 +0100

    Fixed connection string for RDS

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host: process.env.RDS_DB_HOST
-  user : process.env.RDS_USER
-  password: process.env.RDS_PASSWORD
-  database:process.env.RDS_DB_NAME
+  host: process.env.RDS_DB_HOST,
+  user : process.env.RDS_USER,
+  password: process.env.RDS_PASSWORD,
+  database:process.env.RDS_DB_NAME,
   port:process.env.RDS_PORT
 });
 
